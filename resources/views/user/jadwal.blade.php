cdb2ca69d570301c6bee85299f42ebbb76c83894
unknown <aisyah.azkiaqolbi.te18@mhsw.pnj.ac.id>
2022-06-29 15:32:22 +0700

    edit menu user

--- resources/views/user/jadwal.blade.php
+++ resources/views/user/jadwal.blade.php
@@ -232,5 +232,12 @@ data-template="vertical-menu-template-free"
   </table>
 
 </section> --}}
+<script src="{{ asset ('/vendor/purecounter/purecounter.js') }}"></script>
+  <script src="{{ asset ('/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset ('/vendor/glightbox/js/glightbox.min.js') }}"></script>
+  <script src="{{ asset ('/vendor/swiper/swiper-bundle.min.js') }}"></script>
+  <script src="{{ asset ('/vendor/php-email-form/validate.js') }}"></script>
 
+  <!-- Template Main JS File -->
+  <script src="js/main.js"></script>
 </body>
--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -224,5 +224,12 @@
     </div>
   </section>
 
+  <script src="{{ asset ('/vendor/purecounter/purecounter.js') }}"></script>
+  <script src="{{ asset ('/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset ('/vendor/glightbox/js/glightbox.min.js') }}"></script>
+  <script src="{{ asset ('/vendor/swiper/swiper-bundle.min.js') }}"></script>
+  <script src="{{ asset ('/vendor/php-email-form/validate.js') }}"></script>
 
+  <!-- Template Main JS File -->
+  <script src="js/main.js"></script>
 </body>
